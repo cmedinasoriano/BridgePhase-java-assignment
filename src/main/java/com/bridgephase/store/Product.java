9c38866133899a4b46a56a8b62fb16c4c90f9b5e
Christian Medina <cmedina@Christians-MacBook-Pro-2.local>
2017-11-24 16:32:31 -0400

    Defined ProductClass and closes #2

--- src/main/java/com/bridgephase/store/Product.java
+++ src/main/java/com/bridgephase/store/Product.java
@@ -1,5 +1,17 @@
 package com.bridgephase.store;
 
 public class Product {
+	private String upc;
+	private String name;
+	private Float wholesalePrice;
+	private Float retailPrice;
+	private Integer quantity;
 
+	public Product(String upc, String name, float wholesalePrice, float retailPrice, int quantity) {
+		this.upc = upc;
+		this.name = name;
+		this.wholesalePrice = wholesalePrice;
+		this.retailPrice = retailPrice;
+		this.quantity = quantity;
+	}
 }
