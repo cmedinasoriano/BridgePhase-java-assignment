31af6fd769742c4eaed583d46cc2e7537f2a5590
Christian Medina <cmedina@Christians-MacBook-Pro-2.local>
2017-11-24 17:37:43 -0400

    Defined replenish and list methods, and parsed input stream. Closes #3, closes #4 & closes #5.

--- src/main/java/com/bridgephase/store/Inventory.java
+++ src/main/java/com/bridgephase/store/Inventory.java
@@ -1,24 +1,76 @@
 package com.bridgephase.store;
 
 import java.io.InputStream;
+import java.io.UnsupportedEncodingException;
 import java.util.List;
+import java.util.ArrayList;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.util.Scanner;
+import java.util.Collections;
 
 import com.bridgephase.store.interfaces.IInventory;
 
 public class Inventory implements IInventory {
+	
+	List<Product> products = null;
+	
+	public Inventory() {
+		products = new ArrayList<Product>();
+	}
 
 	@Override
 	public void replenish(InputStream inputStream) {
-		// TODO Auto-generated method stub
+		// Use scanner to parse input stream as string line by line
+		Scanner scanner = new Scanner( inputStream );
+		String line = null;
+		
+		// Parse InputStream line by line
+		while( scanner.hasNextLine() ) {
+			line = scanner.nextLine();
+			
+			// Parse and create a product for this line if matches pattern
+			Product product = productFromString( line );
+			if( product != null )
+			{
+				products.add( product );
+			}
+		}
 	}
 
 	@Override
 	public List<Product> list() {
-		// TODO Auto-generated method stub
-		return null;
+		// Returns an unmodifiable Products list
+		return Collections.unmodifiableList(products);
+	}
+	
+	/**
+	 * Creates a product from a string as long as it matches the pattern (string,string,float,float,int)
+	 * 
+	 * @param string the String to parse
+	 * @return a new Product object or null if string doesn't match the pattern
+	 */
+	private Product productFromString( String string ) {
+		
+		// Create regex matcher
+		String pattern = "^ *([a-zA-Z0-9]+), *(\\w+) *, *(\\d+(?:\\.\\d*)?) *, *(\\d+(?:\\.\\d*)?) *, *(\\d+) *$";
+		Matcher m = Pattern.compile(pattern, Pattern.DOTALL).matcher(string);
+		
+		// If matches pattern
+		if( m.matches() ) {
+
+			String upc = m.group(1);
+			String name = m.group(2);
+			float wholesalePrice = Float.parseFloat(m.group(3));
+			float retailPrice = Float.parseFloat(m.group(4));
+			int quantity = Integer.parseInt(m.group(5));
+			
+			// Return a new product
+			return new Product(upc, name, wholesalePrice, retailPrice, quantity);	
+		} else {
+			// Didn't match the pattern
+			return null;
+		}
 	}
 
 }
