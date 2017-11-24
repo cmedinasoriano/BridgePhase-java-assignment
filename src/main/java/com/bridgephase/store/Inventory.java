0a351986d8191f18c25d3926b56adc6dbab1dad3
Christian Medina <cmedina@Christians-MacBook-Pro-2.local>
2017-11-24 15:34:42 -0400

    Implemented Inventory and closes #1

---
+++ src/main/java/com/bridgephase/store/Inventory.java
@@ -0,0 +1,24 @@
+package com.bridgephase.store;
+
+import java.io.InputStream;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.Scanner;
+
+import com.bridgephase.store.interfaces.IInventory;
+
+public class Inventory implements IInventory {
+
+	@Override
+	public void replenish(InputStream inputStream) {
+		// TODO Auto-generated method stub
+	}
+
+	@Override
+	public List<Product> list() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
